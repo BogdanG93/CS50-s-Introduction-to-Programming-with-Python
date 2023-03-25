5e08d5934abcf76bac5e5c0279981093d56b922a
BogdanG93 <bogdan_geagu93@yahoo.com>
2023-03-25 17:46:02 +0200

    first commit

---
+++ Problem Set 0/indoor.py
@@ -0,0 +1,13 @@
+"""Best to use your “indoor voice” sometimes, writing entirely in lowercase.
+In a file called indoor.py, implement a program in Python that prompts the user
+for input and then outputs that same input in lowercase. Punctuation and whitespace
+should be outputted unchanged. You’re welcome, but not required, to prompt the user
+explicitly, as by passing a str of your own as an argument to input.
+
+Here’s how to test your code manually:
+Type HELLO and press Enter. Your program should output hello.
+Type THIS IS CS50 and press Enter. Your program should output this is cs50.
+Type 50 and press Enter. Your program should output 50."""
+
+indoor_voice = input('').lower()
+print(indoor_voice)
